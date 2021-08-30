dd3af5f5ef3c9f0c748b584976617009476b3ddc
Ps1h0 <platonov.liceum@mail.ru>
2021-08-30 13:08:56 +0800

    HW6

--- gateway-server/src/main/java/ru/geekbrains/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/ru/geekbrains/gatewayserver/GatewayServerApplication.java
@@ -2,8 +2,10 @@ package ru.geekbrains.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
--- ms-auth/src/main/java/ru/geekbrains/auth/MsAuthApplication.java
+++ ms-auth/src/main/java/ru/geekbrains/auth/MsAuthApplication.java
@@ -2,8 +2,10 @@ package ru.geekbrains.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "ru.geekbrains")
+@EnableEurekaClient
 public class MsAuthApplication {
 
     public static void main(String[] args) {
